2059573aca8c818b67cc902368e19d7285e253d5
ucpatil <umesh123cpatil@gmail.com>
2026-05-10 22:37:54 +0530

    hopefully notes are added

---
+++ day1/src/TestScanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class TestScanner{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter 2 numbers : ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(a+b);
+	}
+
+}

9d7b05c8b869f487e79a41d069e14354e8989ae7
ucpatil <umesh123cpatil@gmail.com>
2026-05-11 23:01:47 +0530

    ok

---
+++ day1/src/DoubleSum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class DoubleSum{
+	public static void main(String[] args){
+		System.out.println("enter 2 numbers: ");
+		Scanner sc=new Scanner(System.in); 
+		Double num1=sc.nextDouble();
+		Double num2=sc.nextDouble();
+
+		System.out.println("so the multiplication is :"+(num1*num2));
+
+	}
+}
